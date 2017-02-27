1fcfd646ee4ef349f4bf1c65d7f6c5b897ae6c30
Jonas <snillsparv@gmail.com>
2017-02-27 16:06:42 +0100

    Testcommit (har lagt till en testkommentar i MainApp)

--- PayWizard/src/MainApp.java
+++ PayWizard/src/MainApp.java
@@ -7,7 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+//Testkommentar
 public class MainApp extends Application {
 
     public static void main(String[] args) {
