336f5768306efb9878121d23dddb522430a36673
hannacarlssoon <hanna_carlsson_96@hotmail.com>
2017-02-28 12:18:37 +0100

    Fixade mapparna osv, la in historik vyn i shoppingView och gjorde en ny mapp för betalnings vyn

---
+++ src/paymentView/MainApp.java
@@ -0,0 +1,27 @@
+package paymentView;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+//Testkommentar
+public class MainApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("view/Wizard.fxml"));
+        //FXMLLoader fxmlLoader = FXMLLoader.load(getClass().getResource("Wizard.fxml"));
+        //fxmlLoader.setController(this);
+        //Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, 1366, 768);
+
+        stage.setTitle("iMat");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
